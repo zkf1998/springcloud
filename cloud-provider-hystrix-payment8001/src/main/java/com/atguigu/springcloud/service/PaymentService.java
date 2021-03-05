2214a934aeea9c10404b504d66e4506d51b573c5
zkf1998 <1061585651@qq.com>
2021-03-05 16:22:55 +0800

    hystrix

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,14 +13,21 @@ public class PaymentService {
         return "线程池:"+Thread.currentThread().getName()+"paymentInfo_OK,id:"+id+"\t"+"哈哈～";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",
+            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")})
     public String paymentInfo_TimeOut(Integer id){
 
-        int timeNumber=3;
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "线程池:"+Thread.currentThread().getName()+"paymentInfo_TimeOut,id:"+id+"\t"+"哈哈～"+"耗时(秒):"+timeNumber;
+       // int timeNumber=5;
+        int age=10/0;
+//        try {
+//            TimeUnit.SECONDS.sleep(timeNumber);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "线程池:"+Thread.currentThread().getName()+"paymentInfo_TimeOut,id:"+id+"\t"+"哈哈～"+"耗时(秒):";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + " 8001系统繁忙系统报错,请稍后再试id: " + id + "\t" + "( Ĭ ^ Ĭ )";
     }
 }
